f036fabc5f797fac2d594d06b263e2ba7e34eb5a
admin <admin>
2021-08-11 18:45:43 -0400

    fix: all

--- src/main/java/cl/metalcloud/api/NamesController.java
+++ src/main/java/cl/metalcloud/api/NamesController.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/names")
+@CrossOrigin("*")
 public class NamesController {
 
 	private static ArrayList<String> names = new ArrayList<>(Arrays.asList("Pedro","Antonio","Roberto", "Juan", "Luis"));
@@ -29,6 +31,6 @@ public class NamesController {
 	    int index = getRandomNumber(1, names.size()) - 1;
 	    if(index < 0) index = 0;
 	    if(index > names.size()) index = names.size()-1;
-		return names.get(index);
+		return "\"" + names.get(index) + "\"";
 	}
 }
